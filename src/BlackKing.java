b3aa530cf7a9a6fd6f97c0fa062cc05419d0b7b3
Theo Baker <tabsterbaker@gmail.com>
2020-11-19 16:44:29 -0500

    initial commit

---
+++ src/BlackKing.java
@@ -0,0 +1,25 @@
+//import java.util.Random;
+//
+//public class BlackKing extends Piece {
+//	    private static int row;
+//	    private static int column;
+//	    Random rand = new Random();
+//	    public BlackKing(){
+//		        int i = rand.nextInt(4);
+//		        row = i;
+//		        int j = rand.nextInt(4);
+//		        column = j;
+//		    }
+//	    public static int getRow (){
+//		        return row;
+//		    }
+//	    public static int getColumn (){
+//		        return column;
+//		    }
+//	    public void reRandomize(){
+//		        int i = rand.nextInt(4);
+//		        row = i;
+//		        int j = rand.nextInt(4);
+//		        column = j;
+//		    }
+//}
