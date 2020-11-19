60f11a4ada654f4ddb356b86a4337172098dfcb7
Theo Baker <tabsterbaker@gmail.com>
2020-11-19 16:46:47 -0500

    added functionality to Simulation.java

---
+++ src/WhitePiece.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+public class WhitePiece {
+	private int row;
+	private int column;
+	Random rand = new Random();  
+	
+	  public int getRow (){
+	       return row;
+	   }
+	   public int getColumn (){
+	       return column;
+	   }
+	  
+}
